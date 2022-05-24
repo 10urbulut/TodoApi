f89639837a347c87f5a5b0dd685d94f591c32208
Onur Bulut <10urbulut@gmail.com>
2022-05-24 20:36:54 +0300

    Add user model

---
+++ Todo/Core/Entity/User.cs
@@ -0,0 +1,14 @@
+﻿using Todo.Core;
+
+namespace Todo.Models
+{
+    public class User:BaseEntity<int>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
+
+    }
+}
