93e181af5a666b1f5e4d6f6f6cea5617d2103e18
Onur Bulut <10urbulut@gmail.com>
2022-05-24 17:49:19 +0300

    Add; controller helper, hashing helper, security key helper, sign incredential helper,token helper

---
+++ Todo/Core/Helpers/ControllerHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Todo.Core
+{
+
+    public static class ControllerHelpers
+    {
+        public static List<string> GetFullErrorMessage(this ModelStateDictionary modelState)
+        {
+            List<string> messages = new();
+
+            foreach (var entry in modelState)
+            {
+                foreach (var error in entry.Value.Errors)
+                    messages.Add(error.ErrorMessage);
+            }
+            return messages;
+        }
+    }
+}
---
+++ Todo/Core/Helpers/TokenHelper.cs
@@ -0,0 +1,57 @@
+﻿
+
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Todo.Core.DTOs;
+using Todo.Models;
+
+namespace Todo.Core
+{
+
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user);
+    }
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration _configuration { get; }
+        private TokenOptionsDto _tokenOptions;
+        private DateTime _accessTokenExpiration;
+        public JwtHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _tokenOptions = _configuration.GetSection("JWT").Get<TokenOptionsDto>();
+
+        }
+        public AccessToken CreateToken(User user)
+        {
+
+            _accessTokenExpiration = DateTime.Now.AddHours(1);
+            SecurityKey securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            SigningCredentials signingCredentials = SignInCredentialsHelper.CreateSigningCredentials(securityKey);
+            SecurityToken jwt = CreateJwtSecurityToken(signingCredentials);
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new ();
+            string token = jwtSecurityTokenHandler.WriteToken(jwt);
+
+            return new AccessToken
+            {
+                Token = token,
+                Expiration = _accessTokenExpiration
+            };
+
+        }
+
+        public JwtSecurityToken CreateJwtSecurityToken(
+            SigningCredentials signingCredentials)
+        {
+            JwtSecurityToken jwt = new(
+                issuer: _tokenOptions.Issuer,
+                audience: _tokenOptions.Audience,
+                expires: _accessTokenExpiration,
+                notBefore: DateTime.Now,
+                signingCredentials: signingCredentials
+            );
+            return jwt;
+        }
+    }
+}
